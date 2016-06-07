640f5f23343029deae2144ed6d6c8b802d47a878
Nicklas Utgaard <nutgaard@gmail.com>
2016-06-07 22:42:41 +0200

    toggle mode title

--- app/components/urlbar.js
+++ app/components/urlbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {connect} from "./../storeprovider";
 
+const modeTitle = "Toggle interactive/edit mode";
+
 function Urlbar(props, { store }) {
     const onSubmit = (e) => { e.preventDefault(); store.gotoUrl(store.urlbar) };
     const onChange = (e) => { store.setUrlbar(e.target.value); };
@@ -10,7 +12,7 @@ function Urlbar(props, { store }) {
         <form className="urlbar" onSubmit={onSubmit}>
             <input type="text" value={store.urlbar} onChange={onChange}/>
             <button type="submit">GO</button>
-            <button type="button" onClick={toggleMode}>{store.editModeIcon}</button>
+            <button type="button" onClick={toggleMode} title={modeTitle}>{store.editModeIcon}</button>
         </form>
     );
 }
